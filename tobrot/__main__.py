5e99c77677986025e5624234c3d772e95a9e1d4d
Adnan Ahmad <60770759+viper-adnan@users.noreply.github.com>
2020-06-25 10:25:38 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -110,7 +110,7 @@ if __name__ == "__main__" :
         new_join_f,
         filters=~Filters.chat(chats=AUTH_CHANNEL)
     )
-    app.add_handler(group_new_join_handler)
+    app.add_handler(new_join_handler)
     #
     call_back_button_handler = CallbackQueryHandler(
         button
